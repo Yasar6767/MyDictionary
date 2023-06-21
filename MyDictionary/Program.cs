99a524b3f47977807e5d8c0a3534de259d1eb5f3
Yaşar Mehmet Akat <akat434@gmail.com>
2023-06-21 16:08:42 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary myDictionary = new MyDictionary();
+        }
+    }
+
+    class MyDictionary
+    {
+        public MyDictionary()
+        {
+            Dictionary<int, string> Ogrenciler = new Dictionary<int, string>()
+            {
+                {1, "Yaşar"},
+                {2, "Elif"},
+                {3, "Azra"},
+                {4, "Selim"}
+            };
+
+            foreach (var ogrenci in Ogrenciler)
+            {
+                Console.WriteLine("Key{0}, Value{1} "  + ogrenci.Key + " " + ogrenci.Value );
+            }
+        }
+
+    }
+}
---
+++ MyDictionary2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> Ogrenciler = new Dictionary<int, string>();
+            Ogrenciler.Add(1, "Yaşar");
+            Ogrenciler.Add(2, "Elif");
+            Ogrenciler.Add(3, "Azra");
+            Ogrenciler.Add(4, "Selim");
+
+            foreach (var ogrenci in Ogrenciler)
+            {
+                Console.WriteLine(ogrenci);
+            }
+        }
+    }
+}
